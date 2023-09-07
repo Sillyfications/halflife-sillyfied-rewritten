379bd23d157f5aff69d701ecc678ae7c3f714c37
clueless8individual <jerma985zerk@gmail.com>
2023-09-07 12:59:21 +0200

    Initial commit

---
+++ dlls/dbarrel.cpp
@@ -0,0 +1,136 @@
+//Half-Life: Sillyfied Double Barrel Shotgun
+
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "monsters.h"
+#include "weapons.h"
+#include "player.h"
+#include "soundent.h"
+#include "gamerules.h"
+#include "UserMessages.h"
+
+LINK_ENTITY_TO_CLASS(weapon_dbarrel, CDB);
+
+void CDB::Spawn()
+{
+	Precache();
+	SET_MODEL(ENT(pev), "models/w_quad.mdl");
+	m_iId = WEAPON_DBARREL;
+	m_iDefaultAmmo = 2; // ammo on spawn
+	FallInit();
+}
+
+void CDB::Precache()
+{
+	PRECACHE_MODEL("models/v_quad.mdl");
+	PRECACHE_MODEL("models/w_quad.mdl");
+	PRECACHE_MODEL("models/p_quad.mdl");
+	PRECACHE_SOUND("weapons/quad.wav");
+
+	m_iShell = PRECACHE_MODEL("models/shotgunshell.mdl"); // brass shell
+}
+
+bool CDB::GetItemInfo(ItemInfo* p)
+{
+	p->pszName = STRING(pev->classname);
+	p->pszAmmo1 = "buckshot";		   // its a shotgun, of course 12G
+	p->iMaxAmmo1 = BUCKSHOT_MAX_CARRY; // 125
+	p->pszAmmo2 = NULL;
+	p->iMaxAmmo2 = NULL;
+	p->iMaxClip = 2;			  // 2 barrels
+	p->iSlot = 5;				  // 2 = smg, shotgun, crossbow (BUG! sprite doesn't show in game? gotta ask for help)
+	p->iPosition = 0;			  // below m40a1 scopeless		(BUG! same here)
+	p->iFlags = 0;				  // no idea
+	p->iId = m_iId = WEAPON_DBARREL; // errm errm also no idea but must be important
+	p->iWeight = MP5_WEIGHT;
+
+	return true;
+}
+
+bool CDB::Deploy()
+{
+	return DefaultDeploy("models/v_quad.mdl", "models/p_quad.mdl", DB_DRAW, "shotgun");
+}
+
+// tweak some stuff before release
+void CDB::PrimaryAttack()
+{
+	// don't fire underwater
+	if (m_pPlayer->pev->waterlevel == 3)
+	{
+		PlayEmptySound();
+		m_flNextPrimaryAttack = 0.15;
+		return;
+	}
+
+	// don't fire if empty
+	if (m_iClip <= 0)
+	{
+		PlayEmptySound();
+		m_flNextPrimaryAttack = 0.15;
+		return;
+	}
+
+	// nuzzle flash and sound volume
+	m_pPlayer->m_iWeaponVolume = NORMAL_GUN_VOLUME;
+	m_pPlayer->m_iWeaponFlash = NORMAL_GUN_FLASH;
+	m_pPlayer->pev->effects = (int)(m_pPlayer->pev->effects) | EF_MUZZLEFLASH;
+
+	// player "shoot" animation
+	m_pPlayer->SetAnimation(PLAYER_ATTACK1);
+
+	Vector vecSrc = m_pPlayer->GetGunPosition();
+	Vector vecAiming = m_pPlayer->GetAutoaimVector(AUTOAIM_5DEGREES);
+	Vector vecDir = m_pPlayer->FireBulletsPlayer(18, vecSrc, vecAiming, VECTOR_CONE_DOUBLE, 8192, BULLET_PLAYER_BUCKSHOT,
+		1, 13, m_pPlayer->pev, m_pPlayer->random_seed); // 9 per shell
+
+	// play view model animation and firing sound
+	SendWeaponAnim(DB_SHOOT1 + RANDOM_LONG(0, 2));
+	EMIT_SOUND(edict(), CHAN_AUTO, "weapons/quad.wav", 1, ATTN_NORM);
+
+
+
+
+	// recoil
+	m_pPlayer->pev->punchangle.x -= 8;
+	m_iClip--;
+	m_iClip--;
+
+	m_flNextPrimaryAttack = 0.1;
+	m_flTimeWeaponIdle = 1.85;
+}
+
+// tweak some stuff before release
+void CDB::SecondaryAttack()
+{
+	return;
+}
+
+void CDB::Reload()
+{
+	DefaultReload(2, DB_RELOAD, 1.7);
+
+	// Eject the brass while reloading
+	Vector vecShellVelocity = m_pPlayer->pev->velocity + gpGlobals->v_right * RANDOM_FLOAT(250, 500) +
+							  gpGlobals->v_up * RANDOM_FLOAT(250, 500) + gpGlobals->v_forward * 25;
+	EjectBrass(pev->origin + m_pPlayer->pev->view_ofs + gpGlobals->v_up * -12 + gpGlobals->v_forward * 9 +
+				   gpGlobals->v_right * 6,
+		vecShellVelocity, pev->angles.y, m_iShell, TE_BOUNCE_SHELL);
+	EjectBrass(pev->origin + m_pPlayer->pev->view_ofs + gpGlobals->v_up * -12 + gpGlobals->v_forward * 9 +
+				   gpGlobals->v_right * 6,
+		vecShellVelocity, pev->angles.y, m_iShell, TE_BOUNCE_SHELL);
+}
+
+
+void CDB::WeaponIdle()
+{
+	ResetEmptySound();
+
+
+	if (m_flTimeWeaponIdle > UTIL_WeaponTimeBase())
+		return;
+
+
+	SendWeaponAnim(DB_IDLE);
+}
---
+++ dlls/dualelites.cpp
@@ -0,0 +1,129 @@
+#include "extdll.h"
+#include "util.h"
+#include "cbase.h"
+#include "monsters.h"
+#include "weapons.h"
+#include "player.h"
+#include "soundent.h"
+#include "gamerules.h"
+#include "UserMessages.h"
+
+LINK_ENTITY_TO_CLASS(weapon_elite, CElite);
+
+void CElite::Spawn()
+{
+	Precache();
+	SET_MODEL(ENT(pev), "models/w_elite.mdl");
+	m_iId = WEAPON_ELITE;
+	m_iDefaultAmmo = 30; // How much ammo this weapon has on spawn
+	FallInit();			 // get ready to fall down.
+}
+
+void CElite::Precache()
+{
+	PRECACHE_MODEL("models/v_elite.mdl");
+	PRECACHE_MODEL("models/w_elite.mdl");
+	PRECACHE_MODEL("models/p_elite.mdl");
+
+	m_iShell = PRECACHE_MODEL("models/shell.mdl"); // brass shell
+
+	PRECACHE_SOUND("weapons/elite.wav");
+}
+
+bool CElite::GetItemInfo(ItemInfo* p)
+{
+	p->pszName = STRING(pev->classname);
+	p->pszAmmo1 = "9mm"; // Which ammo type this weapon use
+	p->iMaxAmmo1 = _9MM_MAX_CARRY;	 // What's the max ammo quantity for that kind of ammo
+	p->pszAmmo2 = NULL;
+	p->iMaxAmmo2 = NULL;
+	p->iMaxClip = 30; // How many ammo this weapon's clip or magazine has
+	p->iSlot = 1;	  // Which "slot" (column) in the HUD this weapon is located (2 = same slot as HL1 MP5, shotgun, crossbow)
+	p->iPosition = 2; // Which "position" (row) in the HUD this weapon is located (4 = after quad shotgun)
+	p->iFlags = 0;	  // Special flags this weapon has
+	p->iId = m_iId = WEAPON_ELITE;
+	p->iWeight = MP5_WEIGHT; // How much "priority" this weapon has when auto-switch is triggered
+
+	return true;
+}
+
+bool CElite::Deploy()
+{
+	//  The last parameter is the animation set for the player model in thirdperson to use
+	return DefaultDeploy("models/v_elite.mdl", "models/p_elite.mdl", ELITE_DRAW, "glock");
+}
+
+void CElite::PrimaryAttack()
+{
+	// don't fire underwater
+	if (m_pPlayer->pev->waterlevel == 3)
+	{
+		PlayEmptySound();
+		m_flNextPrimaryAttack = 0.15;
+		return;
+	}
+
+	// Don't fire if empty
+	if (m_iClip <= 0)
+	{
+		PlayEmptySound();
+		m_flNextPrimaryAttack = 0.15;
+		return;
+	}
+
+	// Size of the muzzle flash and how much volume in the world the firing sound produces
+	m_pPlayer->m_iWeaponVolume = NORMAL_GUN_VOLUME;
+	m_pPlayer->m_iWeaponFlash = NORMAL_GUN_FLASH;
+	m_pPlayer->pev->effects = (int)(m_pPlayer->pev->effects) | EF_MUZZLEFLASH;
+
+	// player "shoot" animation
+	m_pPlayer->SetAnimation(PLAYER_ATTACK1);
+
+	Vector vecSrc = m_pPlayer->GetGunPosition();
+	Vector vecAiming = m_pPlayer->GetAutoaimVector(AUTOAIM_5DEGREES);
+	Vector vecDir = m_pPlayer->FireBulletsPlayer(2, vecSrc, vecAiming, VECTOR_CONE_5DEGREES, 8192, BULLET_PLAYER_BUCKSHOT,
+		1, 7, m_pPlayer->pev, m_pPlayer->random_seed);//14 dmg
+
+	// Play view model animation and firing sound
+	SendWeaponAnim(ELITE_SHOOT1 + RANDOM_LONG(0, 2));
+	EMIT_SOUND(edict(), CHAN_AUTO, "weapons/elite.wav", 1, ATTN_NORM);
+	
+
+	// Eject the brass
+	Vector vecShellVelocity = m_pPlayer->pev->velocity + gpGlobals->v_right * RANDOM_FLOAT(100, 200) +
+							  gpGlobals->v_up * RANDOM_FLOAT(100, 150) + gpGlobals->v_forward * 25;
+	EjectBrass(pev->origin + m_pPlayer->pev->view_ofs + gpGlobals->v_up * -3 + gpGlobals->v_forward * 16 +
+				   gpGlobals->v_right * -6,
+		vecShellVelocity, pev->angles.y, m_iShell, TE_BOUNCE_SHELL);
+	EjectBrass(pev->origin + m_pPlayer->pev->view_ofs + gpGlobals->v_up * -3 + gpGlobals->v_forward * 16 +
+				   gpGlobals->v_right * 6,
+		vecShellVelocity, pev->angles.y, m_iShell, TE_BOUNCE_SHELL);
+
+	// Punch the camera to simulate recoil
+	m_pPlayer->pev->punchangle.x -= 1.8;
+	// Remove a bullet
+	m_iClip--;
+	m_iClip--;
+	// Next time for attack and weapon idling
+	m_flNextPrimaryAttack = 0.4;//300
+	m_flTimeWeaponIdle = 2;
+}
+
+void CElite::Reload()
+{
+	// Reload 30 bullets, play the AK47_RELOAD animation, reload duration is 2.5 seconds
+	DefaultReload(30, ELITE_RELOAD, 4.7);
+}
+
+
+void CElite::WeaponIdle()
+{
+	ResetEmptySound();
+
+	// Not the time to idle
+	if (m_flTimeWeaponIdle > UTIL_WeaponTimeBase())
+		return;
+
+	// Play idle animation
+	SendWeaponAnim(ELITE_IDLE);
+}
